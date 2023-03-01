2ca0ac8af4c7cf5859c3a0c57a20b785b39a9634
Andrei <andryushik@gmail.com>
2023-03-01 20:53:49 +0100

    react ToDo 2nd part is done

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import styles from './Button.module.css';
 
-function Button() {
-  return <button>Button</button>;
+function Button(props) {
+  const { onClick, children, title, disabled = false } = props;
+
+  return (
+    <button
+      {...props} // если мы какие-то свойства не указали явно, то они тоже будут скопированы
+      className={styles.button}
+      onClick={onClick} // 10 и 11 строку можно удалить если есть 8 строка в ней так же передаются сабмит свойство
+      title={title} //
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
 }
 
 export default Button;
